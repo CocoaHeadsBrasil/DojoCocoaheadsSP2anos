b81ad271483513f9bfa3168c164098b20d4fdc51
Tales Pinheiro de Andrade <tales@talesp.inf.br>
2016-05-21 15:56:52 -0300

    Initial Commit

---
+++ TDD4Square/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDD4Square
+//
+//  Created by Tales pinheiro de Andrade on 21/05/16.
+//  Copyright © 2016 Tales pinheiro de Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
